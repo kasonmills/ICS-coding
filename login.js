97d95572177530ddde3b8d514c7c04c0f7f6988a
kasonmills <97563963+kasonmills@users.noreply.github.com>
2022-03-12 23:26:40 -0700

    Add files via upload

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import { SafeAreaView, StyleSheet, TextInput } from "react-native";
 
 const UselessTextInput = () => {
@@ -32,4 +32,51 @@ const styles = StyleSheet.create({
   },
 });
 
-export default UselessTextInput;
+export default UselessTextInput;*/
+
+import React from 'react';
+import {View, Text, TextInput, StyleSheet} from 'react-native';
+import {Button} from 'react-native-elements';
+
+export default function Login(props){
+
+    const [phoneNumber, onChangePhoneNumber] = React.useState("Please Enter your Phone Number");
+    const [OTP, onChangeOTP] = React.useState(null);
+    fetch('https//dev.stedi.me/twofactorlogin/', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        firstParam: 'yourValue',
+        secondParam: 'yourOtherValue'
+      })
+    });
+    return (
+      <View>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangePhoneNumber}
+          value={phoneNumber}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeOTP}
+          value={OTP}
+          placeholder="Enter your OTP"
+          keyboardType="numeric"
+          />
+        <Button title= "Log In" onPress={()=>props.setUserLoggedIn(true)}></Button>
+      </View>
+    );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
